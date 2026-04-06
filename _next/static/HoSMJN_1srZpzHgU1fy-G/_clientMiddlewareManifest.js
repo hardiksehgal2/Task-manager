de8459b9afa413ce5837433da07961071bc48a95
hardiksehgal2 <hardiksehgal00@gmail.com>
2026-04-07 04:50:34 +0530

    Updates

---
+++ _next/static/HoSMJN_1srZpzHgU1fy-G/_clientMiddlewareManifest.js
@@ -0,0 +1,10 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/auth(\\.json)?[\\/#\\?]?$",
+    "originalSource": "/auth"
+  },
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/dashboard(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json)?[\\/#\\?]?$",
+    "originalSource": "/dashboard/:path*"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
